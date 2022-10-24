c691fa85c0de3e6412c9dff0f42de28306c77917
kphutiyagae <kopanophutiyagae@gmail.com>
2022-10-24 18:39:00 +0200

    CWS-40-save-archive-implementation

--- src/SaveArchive.h
+++ src/SaveArchive.h
@@ -1,25 +1,90 @@
 #ifndef SAVEARCHIVE_H
 #define SAVEARCHIVE_H
 #include <vector>
+#include <string>
 #include "WarEngineMemento.h"
 
+/**
+ * @class SaveArchive
+ * @brief Stores a list of mementos containing simulation state
+*/
 class SaveArchive {
 
 private:
-	vector<WarEngineMemento*> saveList;
+	std::vector<WarEngineMemento*> saveList;
 
 public:
+	/**
+	 * @brief Instantiates the SaveArchive class
+	*/
 	SaveArchive();
-
+	
+	/**
+	 * @brief Adds a new save to the list of stored mementos
+	 * 
+	 * Preconditions:
+	 * 	- newSave must be a WarEngineMemento*
+	 * 
+	 * Postconditions:
+	 * 	- Appends a new 
+	 * @param newSave must be a WarEngineMemento*
+	 * @return void
+	*/
 	void addNewSave(WarEngineMemento* newSave);
 
+	/**
+	 * @brief Returns the last saved memento.
+	 * 
+	 * Postconditions:
+	 * 	- Returns the last element in the saveList vector
+	 * 
+	 * @return WarEngineMemento*
+	 * @exception std::out_of_range save archive is empty
+	 * @exception std::invalid_argument memento with given timestamp is not found in memento list. 
+	*/
 	WarEngineMemento* getLastSave();
 
-	void getSave(string timestamp);
+	/**
+	 * @brief Returns the last saved memento.
+	 * Preconditions:
+	 * 	- timestamp must be a string in date/time format
+	 * 
+	 * Postconditions:
+	 * 	- Returns the last element in the saveList vector
+	 * 
+	 * @param timestamp a UTC time formatted string
+	 * 
+	 * @return WarEngineMemento*
+	 * 
+	 * @exception std::out_of_range save archive is empty
+	*/
+	WarEngineMemento* getSave(std::string timestamp);
+	
+	/**
+	 * @brief Erases all saved mementos from the list of saves.
+	 * Postconditions:
+	 * 	- Clears all elements in the saveList vector
+	 * 
+	 * @return boolean
+	 * 
+	*/
+	bool clearSaveList();
 
-	boolean clearSaveList();
-
-	boolean deleteSave(string timestamp);
+	/**
+	 * @brief Deletes a memento with the matching given timestamp from the list of saved mementos.
+	 * Preconditions:
+	 * 	- timestamp must be a string in date/time format
+	 * 
+	 * Postconditions:
+	 * 	- Removes the element in the saveList vector with a timestamp matching that of the parameter
+	 * 
+	 * @param timestamp a UTC time formatted string
+	 * 
+	 * @return boolean
+	 * 
+	 * @exception std::out_of_range save archive is empty
+	*/
+	bool deleteSave(std::string timestamp);
 };
 
 #endif
--- src/WarEngineMemento.cpp
+++ src/WarEngineMemento.cpp
@@ -1,6 +1,18 @@
 #include "WarEngineMemento.h"
 
-WarEngineMemento(vector<Alliance* players>, vector<Area*> warTheatre) {
-	// TODO - implement WarEngineMemento::WarEngineMemento
-	throw "Not yet implemented";
+WarEngineMemento::WarEngineMemento(WarEngineState* state, std::string timestamp){
+	this->state = state;
+	this->timestamp = timestamp;
+}
+
+void WarEngineMemento::setState(WarEngineState* state){
+	this->state = state;
+}
+
+WarEngineState* WarEngineMemento::getState(){
+	return state;
+}
+
+std::string WarEngineMemento::getTimestamp(){
+	return timestamp;
 }
