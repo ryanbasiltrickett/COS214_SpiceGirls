ebad5cf2d78e586922e2d6a20b542b131801b62b
Ryan Trickett <ryanbasiltrickett@gmail.com>
2022-10-28 18:12:42 +0200

    CWS-73 Outline of Tests

---
+++ src/AddOnTest.cpp
@@ -0,0 +1,105 @@
+#include <stdexcept>
+#include "AddOn.h"
+#include "Piercing.h"
+#include "Armour.h"
+#include "Personnel.h"
+#include "gtest/gtest.h"
+
+namespace {
+
+    // Tests General AddOn Functionality
+
+    // Tests AddOn setValue()
+    // ============ Precondition Testing ============
+    // Test Precondition Negative
+    TEST(AddOnSetValueTest, TestPreconditionNegative) {
+
+    }
+
+    // Test Precondition Positive
+    TEST(AddOnSetValueTest, TestPreconditionPositive) {
+        
+    }
+
+    // Test Precondition Bounds
+    TEST(AddOnSetValueTest, TestPreconditionBounds) {
+        
+    }
+
+    // ============ Positive Testing ============
+    // Test Preconditions Bounds
+    TEST(AddOnSetValueTest, PositiveTesting) {
+        
+    }
+    
+    // Tests AddOn setEntity()
+    // ============ Positive Testing ============
+    // Test Preconditions Bounds
+    TEST(AddOnSetEntityTest, PositiveTesting) {
+        
+    }
+    
+    // Tests Armour AddOn Functionality
+
+    // Tests Armour takeDamage()
+    // ============ Precondition Testing ============
+    // Test Precondition Negative
+    TEST(ArmourTakeDamageTest, TestPreconditionNegative) {
+
+    }
+
+    // Test Precondition Positive
+    TEST(ArmourTakeDamageTest, TestPreconditionPositive) {
+        
+    }
+
+    // Test Precondition Bounds
+    TEST(ArmourTakeDamageTest, TestPreconditionBounds) {
+        
+    }
+
+    // ============ Positive Testing ============
+    // Test Preconditions Bounds
+    TEST(ArmourTakeDamageTest, PositiveTesting) {
+        
+    }
+    
+    // Tests Armour dealDamage()
+    // ============ Positive Testing ============
+    // Test Preconditions Bounds
+    TEST(ArmourDealDamageTest, PositiveTesting) {
+        
+    }
+
+    // Tests Piercing AddOn Functionality
+    
+    // Tests Piercing dealDamage()
+    // ============ Positive Testing ============
+    // Test Preconditions Bounds
+    TEST(PiercingDealDamageTest, PositiveTesting) {
+        
+    }
+    
+    // Tests Piercing takeDamage()
+    // ============ Precondition Testing ============
+    // Test Precondition Negative
+    TEST(PiercingTakeDamageTest, TestPreconditionNegative) {
+
+    }
+
+    // Test Precondition Positive
+    TEST(PiercingTakeDamageTest, TestPreconditionPositive) {
+        
+    }
+
+    // Test Precondition Bounds
+    TEST(PiercingTakeDamageTest, TestPreconditionBounds) {
+        
+    }
+
+    // ============ Positive Testing ============
+    // Test Preconditions Bounds
+    TEST(ArmourTakeDamageTest, PositiveTesting) {
+        
+    }
+}
