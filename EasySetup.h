288bc4a2c4c9561cfd3cadbe5da5e5819b8ae6bf
Simphiwe Nonabe <u21488275@tuks.co.za>
2022-11-01 09:48:35 +0200

    CWS-75 setupSimulation function implementation

--- EasySetup.h
+++ EasySetup.h
@@ -1,21 +1,37 @@
 #ifndef EASYSETUP_H
 #define EASYSETUP_H
+#include <iostream>
+#include <cctype>
 #include <string>
+#include <vector>
+#include "Alliance.h"
+#include "Country.h"
+#include "AquaticType.h"
+#include "AerialType.h"
+#include "TerrainType.h"
+#include "Piercing.h"
+#include "Armour.h"
+#include "PersonnelFactory.h"
+#include "VehicleFactory.h"
+#include "SupportFactory.h"
+#include "KeyPoint.h"
 #include "WarEngine.h"
 #include "SaveArchive.h"
+
+using namespace std;
 
 class EasySetup
 {
     private:
         WarEngine* warEngine;
         SaveArchive* saveArchive;
-
+    
     public:
-        void setupSimulation;
-        void runSimulation;
-        void loadPrevSave;
+        void setupSimulation();
+        void runSimulation();
+        void loadPrevSave();
         void loadSpecificSave(string name);
-        void sameSimulationSetup();
+        void saveSimulationSetup();
 };
 
 #endif
