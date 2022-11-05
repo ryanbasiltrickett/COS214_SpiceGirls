b4f8fbb33b0c3e7f1bdbebddfa76848342f209f3
kphutiyagae <kopanophutiyagae@gmail.com>
2022-11-05 17:49:11 +0200

    Add loadSave(...) and loadSpecificSave(...) manually

--- src/EasySetup.cpp
+++ src/EasySetup.cpp
@@ -222,3 +222,31 @@ void EasySetup::saveSimulationSetup() {
 
 }
 
+void EasySetup::loadPrevSave {
+
+    try{
+        WarEngineMemento* saveFile = SaveArchive->getLastSave();
+
+        WarEngine->loadSave(saveFile->getState());
+    }
+    catch(const std::exception& error){
+
+        std::cerr << error.what() << "\n"; 
+    
+    }
+}
+
+void EasySetup::loadSpecificSave(string name) {
+
+    try{
+        
+        WarEngineMemento* saveFile = SaveArchive->getSave(name);
+
+        WarEngine->loadSave(saveFile->getState());
+    }
+    catch(const std::out_of_range& range_error){
+
+        std::cerr << range_error.what() << "\n"; 
+    
+    }
+}
